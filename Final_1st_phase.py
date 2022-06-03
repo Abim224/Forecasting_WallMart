dce825af72a68d54893b1883182608ef45333d57
Abim224 <92101267+Abim224@users.noreply.github.com>
2022-06-03 21:20:29 +0530

    Update Final_1st_phase.py

--- Final_1st_phase.py
+++ Final_1st_phase.py
@@ -8,7 +8,7 @@ import datetime as dt
 import streamlit as st
 import pandas as pd
 import numpy as np
-from sklearn.metrics import mean_absolute_error, mean_squared_error,r2_score,mean_absolute_percentage_error
+#from sklearn.metrics import mean_absolute_error, mean_squared_error,r2_score,mean_absolute_percentage_error
 from statsmodels.tsa.stattools import adfuller,acf, pacf
 from math import sqrt
 import base64
